d0d4d9821fc1d087dd15ebf19931aab90cd9fd5c
Lucas Quintanilha <lcquintanilha6@hotmail.com>
2023-10-24 18:22:47 -0300

    some update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Navbar from '../components/Navbar/index'
 import PokemonCard from '../components/PokemonCard'
+import { Container, Grid } from '@mui/material'
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
-            <PokemonCard />
+            <Container maxWidth="false">
+                <Grid container>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
